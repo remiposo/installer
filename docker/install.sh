7449bb9462f13981e8675ba78a99fec2c764fcb5
Kiichi Isobe <kiichi.isobe@satori.team>
2021-06-16 14:39:40 +0000

    Add docker

---
+++ docker/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# install docker
+#
+
+set -eu
+
+script_home="$(cd "$(dirname "$0")" && pwd)"
+cd "$script_home"
+
+wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/containerd.io_1.4.6-1_amd64.deb
+wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce-cli_20.10.7~3-0~ubuntu-xenial_amd64.deb
+wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_20.10.7~3-0~ubuntu-xenial_amd64.deb
+
+dpkg -i containerd.io_1.4.6-1_amd64.deb docker-ce-cli_20.10.7~3-0~ubuntu-xenial_amd64.deb docker-ce_20.10.7~3-0~ubuntu-xenial_amd64.deb
+
+groupadd docker
+usermod -aG docker $USER
+newgrp docker
